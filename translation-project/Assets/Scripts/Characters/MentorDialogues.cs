3ac968ce712427160fc0263596cc9ab4daa58a34
Guilherme Dias <guilhermedbelarmino@gmail.com>
2019-11-22 17:01:44 -0300

    Ajustes nas cenas
    Pequenos ajustes nas cenas da teia alimentar, acampamento (antártica) e adição dos mentores da missão 9.

--- translation-project/Assets/Scripts/Characters/MentorDialogues.cs
+++ translation-project/Assets/Scripts/Characters/MentorDialogues.cs
@@ -9,9 +9,13 @@ public class MentorDialogues {
      * Format: MISSIONNUMBER_MENTOR(NUMEROMENTOR)_DIALOGUE(NUMERODIALOGUE)
      */
     public const string sufixo = "_Dialogue";
+
     public const string prefixom002 = "M002_Mentor";
     public const string prefixom002Casinha = "M002_Casinha_Mentor";
+
     public const string prefixom004 = "M004_Mentor";
+
+    public const string prefixom009 = "M009_Mentor";
     
     /* ####### */ 
 
@@ -73,6 +77,33 @@ public class MentorDialogues {
     };
 
     /*
+    * M009 - Paleontologia
+    */
+
+    public static string[] M009_Mentor0 =
+{
+        prefixom009 + "0" + sufixo + "1"
+    };
+    public static string[] M009_Mentor1 =
+    {
+        prefixom009 + "1" + sufixo + "1"
+    };
+    public static string[] M009_Mentor2 =
+    {
+        prefixom009 + "2" + sufixo + "1"
+    };
+    public static string[] M009_Mentor3 =
+    {
+        prefixom009 + "3" + sufixo + "1"
+    };
+    public static string[] M009_Mentor4 =
+    {
+        prefixom009 + "4" + sufixo + "1"
+    };
+
+
+
+    /*
      * return the dialogue from mentor in mission with index in range 0 to vector.length
      */
     public static string GetDialogue(string mission, string mentor, int index)
@@ -116,6 +147,22 @@ public class MentorDialogues {
                         return M004_Mentor3[index];
                     case "Mentor4":
                         return M004_Mentor4[index];
+                    default:
+                        return "Check mentor name.";
+                }
+            case "M009":
+                switch(mentor)
+                {
+                    case "Mentor0":
+                        return M009_Mentor0[index];
+                    case "Mentor1":
+                        return M009_Mentor1[index];
+                    case "Mentor2":
+                        return M009_Mentor2[index];
+                    case "Mentor3":
+                        return M009_Mentor3[index];
+                    case "Mentor4":
+                        return M009_Mentor4[index];
                     default:
                         return "Check mentor name.";
                 }
@@ -166,6 +213,22 @@ public class MentorDialogues {
                         return M004_Mentor3.Length;
                     case "Mentor4":
                         return M004_Mentor4.Length;
+                    default:
+                        return -1;
+                }
+            case "M009":
+                switch(mentor)
+                {
+                    case "Mentor0":
+                        return M009_Mentor0.Length;
+                    case "Mentor1":
+                        return M009_Mentor1.Length;
+                    case "Mentor2":
+                        return M009_Mentor2.Length;
+                    case "Mentor3":
+                        return M009_Mentor3.Length;
+                    case "Mentor4":
+                        return M009_Mentor4.Length;
                     default:
                         return -1;
                 }
