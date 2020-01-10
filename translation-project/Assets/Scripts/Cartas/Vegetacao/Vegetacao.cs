1b8d0dbf4e1575f081090db96ad05469f3a8209c
Lucas V. Oliveira <lucas.o@live.com>
2020-01-09 21:38:43 -0300

    Added Fill Images. Added the 15 vegetables images and their questions. Added hints.

--- translation-project/Assets/Scripts/Cartas/Vegetacao/Vegetacao.cs
+++ translation-project/Assets/Scripts/Cartas/Vegetacao/Vegetacao.cs
@@ -36,45 +36,92 @@ public class Vegetacao : AbstractCardManager
     public GameObject instruction_interface;
 
     //Lucas code
+    private System.Random rand;
+    private enum Vegetais { Planta, Fungo, Alga };
 
-    String[] tiposVegetais = new String[]{
-        "planta", 
-        "fungo", 
-        "alga"
+    private readonly String[] questions = new String[] {
+        "É uma planta?",
+        "É um fungo?",
+        "É uma alga?"
     };
+    private int questionId;
+
+    private T randomChoice<T>(T[] array) {
+        int randIndex = rand.Next(array.Length);
+        return array[randIndex];
+    }
 
     public Image algaImg, plantaImg, fungoImg;
-
-    private int cardsCount; //Contador do número de cards que ja foram passados
+    public GameObject winImg, loseImg;
 
     private void updateCurrentCard(int cardIndex) {
+        //Updates current showing card
         currentImage.sprite = sprites[cardIndex];
         currentImage.name = sprites[cardIndex].name;
-        cardName.text = currentImage.name;//.Substring(0,5);
+        //cardName.text = currentImage.name;
+        //updateRandomQuestion();
+        updateQuestion();
     }
 
     private void updateNextCard(int cardIndex){
-        nextImage.GetComponentInChildren<Image>().sprite = sprites[cardIndex];
-        nextImage.name = sprites[cardIndex].name;
+        //Updates the next showing card
+        if(cardIndex < sprites.Length) {
+            nextImage.GetComponentInChildren<Image>().sprite = sprites[cardIndex];
+            nextImage.name = sprites[cardIndex].name;
+        } else {
+            nextImage.GetComponentInChildren<Image>().sprite = null;
+            nextImage.name = "";
+        }
+    }
+
+    private void updateQuestion() {
+        //Updates the question text according to the selected card
+        if(sprites[cardIndex].name.EndsWith("P"))
+            questionId = 0;
+        else if(sprites[cardIndex].name.EndsWith("F"))
+            questionId = 1;
+        else if(sprites[cardIndex].name.EndsWith("A"))
+            questionId = 2;
+        else
+            throw new NotImplementedException("Question not implemented for " + sprites[cardIndex].name);
+        cardName.text = questions[questionId];
+    }
+
+    // private void updateRandomQuestion() {
+    //     //Write random question in the board and returning the right answer
+    //     questionId = rand.Next(questions.Length);
+    //     cardName.text = questions[questionId];
+    // }
+
+    private Vegetais getCardType(int cardIndex) {
+        //Function to return card type according to its index
+        Vegetais cardType;
+        if(sprites[cardIndex].name.StartsWith("A")) 
+            cardType = Vegetais.Alga;
+        else if(sprites[cardIndex].name.StartsWith("F"))
+            cardType = Vegetais.Fungo;
+        else if(sprites[cardIndex].name.StartsWith("P"))
+            cardType = Vegetais.Planta;
+        else
+            throw new NotImplementedException("Type not implemented for " + sprites[cardIndex].name);
+
+        return cardType;
     }
 
 
-    private void Update()
-    {
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.F1))
-        {
             instruction_interface.SetActive(true);
-        }
 
         if (Input.GetKey(KeyCode.Escape))
-        {
             instruction_interface.SetActive(false);
-        }
     }
 
     // initialize after button click on instruction
-    public void Initialize()
-    {
+    public void Initialize() {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        rand = new System.Random(); //Inits random number generator
+
         sprites = Shuffle<Sprite>(sprites);
 
         alimentosCestaList = new List<GameObject>();
@@ -84,33 +131,12 @@ public class Vegetacao : AbstractCardManager
         fungoImg.fillAmount = 1;
 
         cardIndex = 0;
-        isDone = false;
-        cardsCount = 0;
+        //isDone = false;
 
-        //kcalBar.fillAmount = 0f;
-
-        // currentImage.sprite = sprites[cardIndex];
-        // currentImage.name = sprites[cardIndex].name;
-        // cardName.text = currentImage.name.Substring(0,5);
         updateCurrentCard(cardIndex);
-
-
-        Debug.Log(cardName.text);
-
-        //nextImage.GetComponentInChildren<Image>().sprite = sprites[cardIndex + 1];
-        //nextImage.name = sprites[cardIndex + 1].name;
         updateNextCard(cardIndex + 1);
 
         initialPosition = currentImage.transform.parent.position;
-
-        // set initialized from alimentos on inventory to false
-        for (int i = 0; i < alimentosCesta.Length; i++)
-        {
-            alimentosCesta[i].GetComponentInChildren<AlimentosInventarioController>().initialized = false;
-        }
-
-        // show first hint
-        minijogosDicas.SetHintByIndex(cardIndex);
 
         resetButton.interactable = true;
         backButton.interactable = true;
@@ -119,264 +145,89 @@ public class Vegetacao : AbstractCardManager
     override public void CheckLike()
     {
         // do something
+        if(checkAnswer(true))
+            Debug.Log("Resposta certa!");
+        else
+            Debug.Log("Resposta errada!");
+
+
         Transform cardImage = currentImage.GetComponentInChildren<Image>().transform;
 
-        //Instantiate(cardImage, currentImage.transform.position, Quaternion.identity, alimentos);
-
-        // var alimentoCesta = Instantiate(alimentoCestaPrefab, currentImage.transform.position, Quaternion.identity, alimentos);
-        // alimentoCesta.name = currentImage.name;
-        // alimentoCesta.GetComponent<Image>().sprite = currentImage.GetComponentInChildren<Image>().sprite;
-        // alimentoCesta.GetComponent<Image>().preserveAspect = true;
-
-        // alimentosCestaList.Add(alimentoCesta);
-
-        // for (int i = 0; i < alimentosCestaIndex; i++)
-        // {
-        //     //Debug.Log(i + " " + alimentosCesta[i].GetComponentInChildren<AlimentosInventarioController>().initialized);
-        //     if (!alimentosCesta[i].GetComponentInChildren<AlimentosInventarioController>().initialized)
-        //     {
-        //         alimentosCesta[i].GetComponentsInChildren<Image>()[1].color = new Color(1, 1, 1, 1);
-        //         alimentosCesta[i].GetComponentsInChildren<Image>()[1].sprite = currentImage.GetComponentInChildren<Image>().sprite;
-        //         alimentosCesta[i].GetComponentsInChildren<Image>()[1].preserveAspect = true;
-        //         alimentosCesta[i].GetComponentInChildren<Button>().interactable = true;
-        //         alimentosCesta[i].GetComponentInChildren<AlimentosInventarioController>().initialized = true;
-        //         alimentosCesta[i].gameObject.name = currentImage.name;
-
-        //         //Debug.Log("adicionado na pos: " + i);
-        //         // exit for loop
-        //         i = alimentosCestaIndex + 1;
-        //     }
-        // }
-
-        CheckCalories(currentImage.name, true);
         NextCard();
     }
 
     override public void CheckDislike()
     {
         // do something else
+        if(checkAnswer(false))
+            Debug.Log("Resposta certa!");
+        else
+            Debug.Log("Resposta errada!");
         NextCard();
     }
 
-    public void CheckCalories(string cardName, bool add)
-    {
-        switch (cardName.ToLower())
-        {
-            case "abacate":
-                if (add)
-                    kcal += 160;
-                else
-                    kcal -= 160;
-                break;
-            case "ameixa seca":
-                if (add)
-                    kcal += 240;
-                else
-                    kcal -= 240;
-                break;
-            case "amendoas":
-                if (add)
-                    kcal += 579;
-                else
-                    kcal -= 579;
-                break;
-            case "banana":
-                if (add)
-                    kcal += 98;
-                else
-                    kcal -= 98;
-                break;
-            case "barrinha de cereal":
-                if (add)
-                    kcal += 86;
-                else
-                    kcal -= 86;
-                break;
-            case "batata doce":
-                if (add)
-                    kcal += 86;
-                else
-                    kcal -= 86;
-                break;
-            case "cenoura":
-                if (add)
-                    kcal += 36;
-                else
-                    kcal -= 36;
-                break;
-            case "chocolate":
-                if (add)
-                    kcal += 139;
-                else
-                    kcal -= 139;
-                break;
-            case "figo":
-                if (add)
-                    kcal += 249;
-                else
-                    kcal -= 249;
-                break;
-            case "agua":
-                if (add)
-                    kcal += 0;
-                else
-                    kcal -= 0;
-                break;
-            case "laranja":
-                if (add)
-                    kcal += 47;
-                else
-                    kcal -= 47;
-                break;
-            case "leite de soja":
-                if (add)
-                    kcal += 82;
-                else
-                    kcal -= 82;
-                break;
-            case "leite desnatado":
-                if (add)
-                    kcal += 63;
-                else
-                    kcal -= 63;
-                break;
-            case "maca":
-                if (add)
-                    kcal += 52;
-                else
-                    kcal -= 52;
-                break;
-            case "melancia":
-                if (add)
-                    kcal += 30;
-                else
-                    kcal -= 30;
-                break;
-            case "pao":
-                if (add)
-                    kcal += 126.5f;
-                else
-                    kcal -= 126.5f;
-                break;
-            case "queijo cheddar":
-                if (add)
-                    kcal += 402.66f;
-                else
-                    kcal -= 402.66f;
-                break;
-            case "queijo mussarela":
-                if (add)
-                    kcal += 318;
-                else
-                    kcal -= 318;
-                break;
-            case "semente de abobora":
-                if (add)
-                    kcal += 559;
-                else
-                    kcal -= 559;
-                break;
-            case "suco laranja":
-                if (add)
-                    kcal += 54.45f;
-                else
-                    kcal -= 54.45f;
-                break;
-            default:
-                    kcal += 0;
-                break;
+    private bool checkAnswer(bool answer) {
+        //Check if the question type and the card type matches
+        Vegetais currentCardType = getCardType(cardIndex);
+        Vegetais chosenCardType = (Vegetais)questionId;
+        bool rightAnswer = answer == (currentCardType == chosenCardType);
+
+        if(!rightAnswer) { //If it is the wrong answer...
+            switch(currentCardType) {
+                case Vegetais.Alga:
+                    algaImg.fillAmount -= 0.4f;
+                    if(algaImg.fillAmount <= 0)
+                        doLose();
+                    break;
+                case Vegetais.Fungo:
+                    fungoImg.fillAmount -= 0.4f;
+                    if(fungoImg.fillAmount <= 0)
+                        doLose();
+                    break;
+                case Vegetais.Planta:
+                    plantaImg.fillAmount -= 0.4f;
+
+                    break;
+                default:
+                    break;
+            }
         }
 
-        // normalize
-        //kcalBar.fillAmount = kcal / MAX_KCAL;
+        return rightAnswer;       
+    }
 
-        Debug.Log("Você está levando " + kcal + "/" + MAX_KCAL + " kcal");
 
-        // cesta cheia, não pode colocar mais comida.
-        // if (kcalBar.fillAmount == 1)
-        // {
-        //     isDone = true;
-        //     //likeButton.interactable = false;
-        //     Debug.Log("Atingido o máximo de calorias");
+    private bool checkWin() {
+        //Check if the player won the game
+        return cardIndex >= sprites.Length;
+    }
 
-        //     satisfeitoButton.interactable = true;
-        // }
-        // else
-        //     satisfeitoButton.interactable = false;
+    private bool checkLose() {
+        //Checks if the user lose the game
+        return algaImg.fillAmount <= 0 || plantaImg.fillAmount <= 0 || fungoImg.fillAmount <= 0;
+    }
+
+    private void doWin() { //Routine to happen once the user wins
+        winImg.SetActive(true);
+        Debug.Log("Você ganhou!");
+    }
+
+    private void doLose() { //Routine to happen once the user wins
+        loseImg.SetActive(true);
+        Debug.Log("Você perdeu!");
     }
 
     public void NextCard() {
         cardIndex++;
 
-        if(minijogosDicas.hints.Length > 0)
-            minijogosDicas.SetHintByIndex(cardIndex);
-
-        if (cardIndex < sprites.Length) {
-            // currentImage.sprite = nextImage.sprite;
-            // currentImage.name = sprites[cardIndex].name;
-            // cardName.text = currentImage.name;
+        if(checkLose())
+            doLose();
+        else if(checkWin())
+            doWin();
+        else {
             updateCurrentCard(cardIndex);
-
-            Debug.Log(cardName.text);
-
-            if (cardIndex < sprites.Length - 1) {
-                updateNextCard(cardIndex + 1);
-                //nextImage.sprite = sprites[cardIndex + 1];
-                //nextImage.name = sprites[cardIndex + 1].name;
-            } else {
-                Debug.Log("fim das cartas... Começando de novo");
-                cardIndex = -1;
-                updateNextCard(cardIndex + 1);
-                //nextImage.sprite = sprites[cardIndex + 1];
-                //nextImage.name = sprites[cardIndex + 1].name;
-            }
-        }
-
-        ResetPosition();
-    }
-
-    public void ConfirmRemover() {
-        Debug.Log(EventSystem.current.currentSelectedGameObject.transform.parent.name);
-    }
-
-    public void RemoverAlimentoCesta(int index) {
-        alimentosCesta[index].GetComponentsInChildren<Image>()[1].sprite = null;
-        alimentosCesta[index].GetComponentsInChildren<Image>()[1].color = new Color(1, 1, 1, 0);
-        alimentosCesta[index].GetComponentInChildren<Button>().interactable = false;
-        alimentosCesta[index].GetComponentInChildren<AlimentosInventarioController>().initialized = false;
-
-        try
-        {
-            Debug.Log("alimento: " + alimentosCesta[index].gameObject.name);
-            var result = alimentosCestaList.Find(x => x.name.Contains(alimentosCesta[index].gameObject.name));
-            Debug.Log("resultado da lista: " + result.name);
-            result.GetComponent<Image>().enabled = false;
-
-            CheckCalories(result.name, false);
-
-            alimentosCestaList.Remove(result);
-        }
-        catch (Exception ex)
-        {
-            Debug.Log("Não encontrado na cesta. Stacktrace => " + ex.StackTrace);
-        }
-
-        // pode escolher outro alimento para colocar na cesta
-        // if(kcalBar.fillAmount < 1)
-        // {
-        //     isDone = false;
-        //     likeButton.interactable = true;
-        //     dislikeButton.interactable = true;
-        // }
-
-        //ShiftArray(index);
-    }
-
-    public void ShiftArray(int index)
-    {
-        for (int i = index - 1; i < alimentosCestaIndex - 1; i--)
-        {
-            alimentosCesta[i] = alimentosCesta[i + 1];
+            updateNextCard(cardIndex + 1);
+            ResetPosition();
         }
     }
 
@@ -403,7 +254,8 @@ public class Vegetacao : AbstractCardManager
 
     public void ResetScene()
     {
-        SceneManager.LoadScene(ScenesNames.M002Homeostase);
+        SceneManager.LoadScene(ScenesNames.M010TiposVegetacaoScene);
+        //precisa descobrir de onde isso
     }
 
     public void ReturnToUshuaia()
