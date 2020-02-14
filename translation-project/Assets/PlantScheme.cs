2f7b11eedf85396a45bd7b7ec49a547e9a1df51b
Lucas V. Oliveira <lucas.o@live.com>
2020-02-14 13:35:57 -0300

    updated m010

--- translation-project/Assets/PlantScheme.cs
+++ translation-project/Assets/PlantScheme.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantScheme : MonoBehaviour
 {
-    //public GameObject plantSchemeObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject canvasGo;
+    bool allowHide = false;
+    void Start() {
+        canvasGo = transform.GetChild(0).gameObject;
+    }    
 
-    // Update is called once per frame
+
     void Update()
-    {
-        
+    {       
+        if (Input.GetMouseButtonUp(0) || Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.Return)) {
+            if(allowHide) Hide();
+        }
     }
 
     public void Show() {
-        Debug.Log("SHOWWWWW");
-        //plantSchemeObject.SetActive(true);
+        canvasGo.SetActive(true);
+        //Must generate a delay, otherwise the screen will show and imediatelly hide
+        StartCoroutine(allowHideAfterDelay(3));   
+    }
+
+    IEnumerator allowHideAfterDelay(int secs) {
+        yield return new WaitForSeconds(secs);
+        allowHide = true;
+    }
+    public void Hide() {
+        canvasGo.SetActive(false);
+        allowHide = false;
     }
 }
