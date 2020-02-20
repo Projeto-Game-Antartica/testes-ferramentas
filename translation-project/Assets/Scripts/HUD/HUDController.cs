4f846336031f1bdb595dc64abe57d14987cf616c
Guilherme Dias <guilhermedbelarmino@gmail.com>
2020-02-20 19:02:12 -0300

    Ajustes Baleias
    Novos ajustes de acessibilidade e jogabilidade da missão da baleia

--- translation-project/Assets/Scripts/HUD/HUDController.cs
+++ translation-project/Assets/Scripts/HUD/HUDController.cs
@@ -56,8 +56,7 @@ public class HUDController : AbstractScreenReader {
 
     private void Start()
     {
-        Debug.Log(ReadableTexts.instance.GetReadableText(missionAudiodescriptionKey, LocalizationManager.instance.GetLozalization()));
-        ReadText(ReadableTexts.instance.GetReadableText(missionAudiodescriptionKey, LocalizationManager.instance.GetLozalization()));
+        ReadInstructionAudiodescrition(missionNumber);
 
         Debug.Log(PlayerPrefs.GetInt("InstructionInterface", 0));
 
@@ -143,7 +142,7 @@ public class HUDController : AbstractScreenReader {
             }
             else
             {
-                Debug.Log("SimpleCharacter");
+                //Debug.Log("SimpleCharacter");
                 simpleCharacterController.diagUI.EndDialogue(VD.nodeData);
                 inGameOption.SetActive(false);
             }
@@ -183,6 +182,9 @@ public class HUDController : AbstractScreenReader {
         instructionInterface.SetActive(false);
 
         ShowMap(true);
+
+        Debug.Log(ReadableTexts.instance.GetReadableText(missionAudiodescriptionKey, LocalizationManager.instance.GetLozalization()));
+        ReadText(ReadableTexts.instance.GetReadableText(missionAudiodescriptionKey, LocalizationManager.instance.GetLozalization()));
 
         iniciarButton.gameObject.SetActive(false);
         fecharButton.gameObject.SetActive(true);
