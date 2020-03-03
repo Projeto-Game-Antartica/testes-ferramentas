e7beb575343c3449b169974f58f07acefbc5947a
Lucas V. Oliveira <lucas.o@live.com>
2020-03-03 17:22:48 -0300

    Changed the way dialog balloon are colored to PlayerPreferences flag

--- translation-project/Assets/DialogMentorBalloon.cs
+++ translation-project/Assets/DialogMentorBalloon.cs
@@ -2,24 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class DialogMentorBalloon : MonoBehaviour
 {
     public string MentorName;
-    private string mentorId;
 
     // Start is called before the first frame update
     void Start()
     {
-        string sceneName = SceneManager.GetActiveScene().name;
-        mentorId = sceneName + "_" + MentorName + "_DONE";
 
-        if(PlayerPrefs.GetInt(mentorId, 0) == 1)
-            SetDone();
+        switch(MentorName) {
+            case "Mentor0":
+                if(PlayerPreferences.M010_Mentor0_Talked)
+                    SetDone();
+                break;
+
+            case "Mentor3":
+                if(PlayerPreferences.M010_Mentor3_Talked)
+                    SetDone();
+                break;
+
+            default:
+                throw new NotImplementedException("Mentor name not implemented: " + MentorName);
+        }
     }
 
     public void SetDone() {
-        PlayerPrefs.SetInt(mentorId, 1);
+        switch(MentorName) {
+            case "Mentor0":
+                PlayerPreferences.M010_Mentor0_Talked = true;
+                break;
+
+            case "Mentor3":
+                PlayerPreferences.M010_Mentor3_Talked = true;
+                break;
+
+            default:
+                throw new NotImplementedException("Mentor name not implemented: " + MentorName);
+        }
         gameObject.GetComponent<SpriteRenderer>().color = new Color(0.4f, 1, 0.4f);
     }
 }
