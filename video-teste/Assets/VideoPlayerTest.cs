a72a5affb7efd909fcbe2014b33f67c19c6302ba
Guilherme Dias <guilhermedbelarmino@gmail.com>
2020-03-10 20:05:34 -0300

    Sample project for video execution test
    Sample project trying to execute videos from server, youtube, gdrive, etc.

---
+++ video-teste/Assets/VideoPlayerTest.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoPlayerTest : MonoBehaviour
+{
+    public RawImage image;
+
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+
+        //Add VideoPlayer to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Add AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+
+        //We want to play from video clip not from url
+
+        //videoPlayer.source = VideoSource.VideoClip;
+
+        // Vide clip from Url
+        videoPlayer.source = VideoSource.Url;
+        //videoPlayer.url = "https://youtu.be/7IG5kRFIMZw";
+        //videoPlayer.url = "http://acessivel.ufabc.edu.br/antartica/videos/aves.avi";
+        //videoPlayer.url = "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4";
+        //videoPlayer.url = "https://drive.google.com/file/d/14x-B2rfRkMQvUOTGcMv_dP6yVs5oW6Dj/view?usp=sharing";
+        videoPlayer.url = "https://drive.google.com/file/d/1BX0POJf5bZXKz4guz2VdLc3saAZtif0j/view?usp=sharing";
+
+
+        //Set Audio Output to AudioSource
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //Set video To Play then prepare Audio to prevent Buffering
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("preparing video..");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+        //Assign the Texture from Video to RawImage to be displayed
+        image.texture = videoPlayer.texture;
+
+        //Play Video
+        videoPlayer.Play();
+
+        //Play Sound
+        audioSource.Play();
+
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+
+        Debug.Log("Done Playing Video");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+ 
