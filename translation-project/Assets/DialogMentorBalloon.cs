2e0e41cee3ae8aa8b1b95656eaffbf597f4a21d1
Lucas V. Oliveira <lucas.o@live.com>
2020-03-13 12:51:18 -0300

    Updated mentors dialog balloon routines

--- translation-project/Assets/DialogMentorBalloon.cs
+++ translation-project/Assets/DialogMentorBalloon.cs
@@ -7,63 +7,21 @@ using System;
 public class DialogMentorBalloon : MonoBehaviour
 {
     public string MentorName;
+    public SpriteRenderer DialogBalloon;
+
+    private string mentorId; //Unique ID per mission per mentor
 
     // Start is called before the first frame update
     void Start()
     {
-
-        // switch(MentorName) {
-        //     case "Mentor0":
-        //         if(PlayerPreferences.M010_Mentor0_Talked)
-        //             SetDone();
-        //         break;
-
-        //     case "Mentor3":
-        //         if(PlayerPreferences.M010_Mentor3_Talked)
-        //             SetDone();
-        //         break;
-
-        //     default:
-        //         throw new NotImplementedException("Mentor name not implemented: " + MentorName);
-        // }
-        // Debug.Log("KEYSSSSSSS");
-        // foreach(string key in PlayerPreferences.DoneDialogs.Keys) {
-        //     Debug.Log("Key: " + key);
-        // }
-        if(MentorName != "" && PlayerPreferences.DoneDialogs.ContainsKey(MentorName) && PlayerPreferences.DoneDialogs[MentorName])
-            SetDone(MentorName);
-    }
-    
-    public void SetDone(string mentorName) {
-        // switch(MentorName) {
-        //     case "Mentor0":
-        //         PlayerPreferences.M010_Mentor0_Talked = true;
-        //         break;
-
-        //     case "Mentor3":
-        //         PlayerPreferences.M010_Mentor3_Talked = true;
-        //         break;
-
-        //     default:
-        //         throw new NotImplementedException("Mentor name not implemented: " + MentorName);
-        // }
-        PlayerPreferences.DoneDialogs[MentorName] = true;
-        gameObject.GetComponent<SpriteRenderer>().color = new Color(0.4f, 1, 0.4f);
+        mentorId = MentorName + "_" + SceneManager.GetActiveScene().name;
+        
+        if(PlayerPreferences.DoneDialogs.ContainsKey(mentorId) && PlayerPreferences.DoneDialogs[mentorId])
+            SetDialogDone();
     }
 
-    // public void SetDone() {
-    //     switch(MentorName) {
-    //         case "Mentor0":
-    //             PlayerPreferences.M010_Mentor0_Talked = true;
-    //             break;
-
-    //         case "Mentor3":
-    //             PlayerPreferences.M010_Mentor3_Talked = true;
-    //             break;
-
-    //         default:
-    //             throw new NotImplementedException("Mentor name not implemented: " + MentorName);
-    //     }
-    //     gameObject.GetComponent<SpriteRenderer>().color = new Color(0.4f, 1, 0.4f);
-    // }
+    public void SetDialogDone() {
+        PlayerPreferences.DoneDialogs[mentorId] = true;
+        DialogBalloon.color = new Color(0.4f, 1, 0.4f);
+    }
 }
