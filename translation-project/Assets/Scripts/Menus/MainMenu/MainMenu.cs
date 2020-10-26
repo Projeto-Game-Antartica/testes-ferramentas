c419a88ae436ce0f9994baf6bb6fd034f352ef1d
Lucas V. Oliveira <lucas.o@live.com>
2020-10-26 13:49:20 -0300

    Added all missions start code lines

--- translation-project/Assets/Scripts/Menus/MainMenu/MainMenu.cs
+++ translation-project/Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -117,8 +117,11 @@ public class MainMenu : AbstractScreenReader
 
     public IEnumerator LoadingScreen()
     {
-        //async = SceneManager.LoadSceneAsync(ScenesNames.M004Ship);
         async = SceneManager.LoadSceneAsync(ScenesNames.M002Ushuaia);
+        //async = SceneManager.LoadSceneAsync(ScenesNames.M004Ship);
+        //async = SceneManager.LoadSceneAsync(ScenesNames.M010Camp);
+        //async = SceneManager.LoadSceneAsync(ScenesNames.M009Camp);
+        
 
         loadScreenObject.SetActive(true);
 
